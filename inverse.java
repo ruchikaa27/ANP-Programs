feb40280839dbb4522c382b510976749f31516e4
ruchikaa27 <ruchikaraj272005@gmail.com>
2025-06-26 11:38:12 +0530

    Add files via upload

---
+++ inverse.java
@@ -0,0 +1,12 @@
+package anudip;
+
+public class inverse {
+	public static void main(String args[]) {
+	for(int col=3;col>=1;col--) {
+		for(int row=1;row<=col;row++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+	}
+}
