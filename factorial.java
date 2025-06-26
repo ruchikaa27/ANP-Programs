c218d2f7e2c146574701f37f251a493b3a24fea9
ruchikaa27 <ruchikaraj272005@gmail.com>
2025-06-26 23:01:57 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.scanner;
+package anudip;
+
+public class factorial {
+	public stativ void main(String args[]) {
+		Scanner var1=new scanner(system.in)
+				System.out.println("enter the no";)
+				int var2 = var1.nextInt();
+		int var3=1;
+		
+		for(int var4=1;var4<var2;var4++) {
+			var3=var3*var4;
+		}
+		System.out.println(var3);
+	} 
+
+}
